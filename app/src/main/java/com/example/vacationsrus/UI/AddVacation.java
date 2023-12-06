4e632aa8a95e95db70bc8f26caf97839bd71be45
Luke Giles <luke.giles@cruciallearning.com>
2023-12-05 17:17:49 -0700

    part B3 d, start date before end date

--- app/src/main/java/com/example/vacationsrus/UI/AddVacation.java
+++ app/src/main/java/com/example/vacationsrus/UI/AddVacation.java
@@ -2,7 +2,6 @@ package com.example.vacationsrus.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +14,11 @@ import com.example.vacationsrus.R;
 import com.example.vacationsrus.database.Repository;
 import com.example.vacationsrus.entities.Vacation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class AddVacation extends AppCompatActivity {
     EditText vacationTitle;
     EditText vacationHotel;
@@ -24,10 +28,10 @@ public class AddVacation extends AppCompatActivity {
     String vacationHotelText;
     String vacationStartDateText;
     String vacationEndDateText;
+    Date vacayStartDateDate;
+    Date vacayEndDateDate;
     DateValidator dateValidator;
-    String dateValidationError;
 
-    Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,16 +49,29 @@ public class AddVacation extends AppCompatActivity {
             vacationStartDateText = vacationStartDate.getText().toString();
             vacationEndDate = findViewById(R.id.editTextEndDate);
             vacationEndDateText = vacationEndDate.getText().toString();
-            if (dateValidator.isValid(vacationStartDateText) && dateValidator.isValid(vacationEndDateText)) {
-                Repository repo = new Repository(getApplication());
-                Vacation vacation = new Vacation(vacationTitleText, vacationHotelText, vacationStartDateText, vacationEndDateText, 1);
-                repo.insert(vacation);
 
-                Intent intent = new Intent(AddVacation.this, Vacations.class);
-                startActivity(intent);
+            if (dateValidator.isValid(vacationStartDateText) && dateValidator.isValid(vacationEndDateText)) {
+                try {
+                    vacayStartDateDate = new SimpleDateFormat("yyyy-MM-dd").parse(vacationStartDateText);
+                    vacayEndDateDate = new SimpleDateFormat("yyyy-MM-dd").parse(vacationEndDateText);
+
+                    if (vacayStartDateDate.compareTo(vacayEndDateDate) > 0) {
+                        Toast.makeText(getApplicationContext(), "The start date must be before the end date", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Repository repo = new Repository(getApplication());
+                        Vacation vacation = new Vacation(vacationTitleText, vacationHotelText, vacationStartDateText, vacationEndDateText, 1);
+                        repo.insert(vacation);
+
+                        Intent intent = new Intent(AddVacation.this, Vacations.class);
+                        startActivity(intent);
+                    }
+                } catch (ParseException e) {
+
+                }
             } else {
                 Toast.makeText(getApplicationContext(), "Please use the correct date format", Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
+
